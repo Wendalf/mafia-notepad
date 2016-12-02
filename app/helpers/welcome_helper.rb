485134276e608f424f89b9e1078e7c6be10bae48
Wendy Xu <guangw.xu@gmail.com>
2016-12-02 14:46:22 -0500

    added name attribute to User, set up applicationHelper for devise signin/signup form to be used in other view page

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,11 @@
 module WelcomeHelper
+  def greetings
+    if current_user && !current_user.name.nil? 
+      "Hello #{current_user.name}!"
+    elsif current_user
+      "Hello #{current_user.email}!"
+    else
+      "Welcome to Mafia Notepad"
+    end
+  end
 end
