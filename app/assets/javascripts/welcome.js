bac8abd28176a4098aae009413d8292cf0bc3d74
Wendy Xu <guangw.xu@gmail.com>
2016-12-04 16:27:55 -0500

    added back button on hiddengames div

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -36,11 +36,19 @@ $(function(){
         data.forEach(function(game, index, data) {
           html += `<p>Game ${index+1}: ${game.capacity} players | ${game.game_status}</p>`;
         });
+        html += "<button type='button' class='btn btn-outline-primary'>Back</button>"
         $games.html(html);
         $("div.buttons").fadeOut("slow", function() {
           $("div#hiddengames").fadeIn("slow");
+
         });
       });
+  });
+
+  $("div#hiddengames").on("click", "button", function() {
+    $("div#hiddengames").fadeOut("slow", function() {
+      $("div.buttons").fadeIn("slow");
+    });
   });
 
 });
